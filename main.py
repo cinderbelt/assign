1bdc0510d62c4401ba9fb025b5ef6b603cda3489
cinderbelt <2krok@snu.ac.kr>
2022-05-27 17:47:49 +0900

    최적해 도출 알고리즘 구현 (가장 무식한 방법으로..)

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import pandas as pd
 from random import randint
+import permuting as pm
 
 n = int(input('2부터 6 사이의 정수를 입력하세요: '))
 main_list = [[randint(1, 100) for j in range(n)] for i in range(n)]
 df = pd.DataFrame(main_list,
-    columns=["작업" + str(i + 1) for i in range(n)],
-    index=["기계" + chr(65 + i) for i in range(n)])
+                  columns=["작업" + str(i + 1) for i in range(n)],
+                  index=["기계" + chr(65 + i) for i in range(n)])
 print(df)
-print('esther')
+pm.permutation(n)
+permutation_list=pm.c
+sum=0
+sum_list=[]
+for j in range(len(permutation_list)):
+    for i in range(n):
+        sum+=int(df.iloc[i,permutation_list[j][i]-1])
+    sum_list.append(sum)
+    sum=0
+
+print(permutation_list)
+print(sum_list)
+min= min(sum_list)
+print(min)
+print(sum_list.index(min))
+print(permutation_list[sum_list.index(min)])
