9a30a1845597d6f273900cdaa5bb1bf0adb45064
cinderbelt <2krok@snu.ac.kr>
2022-05-31 20:38:06 +0900

    현준이가 시킨대로 함 ^^;

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import pandas as pd
 from random import randint
-import permuting as pm
+import math
+import random
+def permutation(n):
+    global c
+    c=[]
+    while len(c)<math.factorial(n):
+        a=list(range(1,n+1))
+        b=[]
+        while len(b)<n:
+            e= a[random.randint(0,n-1)]
+            if e not in b:
+                b.append(e)
+        if b not in c:
+            c.append(b)
+        b=[]
 
 n = int(input('2부터 6 사이의 정수를 입력하세요: '))
 main_list = [[randint(1, 100) for j in range(n)] for i in range(n)]
@@ -8,8 +22,8 @@ df = pd.DataFrame(main_list,
                   columns=["작업" + str(i + 1) for i in range(n)],
                   index=["기계" + chr(65 + i) for i in range(n)])
 print(df)
-pm.permutation(n)
-permutation_list=pm.c
+permutation(n)
+permutation_list=c
 sum=0
 sum_list=[]
 for j in range(len(permutation_list)):
